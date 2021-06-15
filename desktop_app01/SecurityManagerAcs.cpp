1c7d7643f8301eef80f3d0248a4226f70713691f
김규운/책임연구원/미래기술센터 CKyuwoon.kimM표준(연)미디어표준Task(kyuwoon.kim@lge.com) <kyuwoon.kim@lge.com>
2021-06-15 17:23:37 +0900

    Add: apply TLS 1.3 on connection.

---
+++ desktop_app01/SecurityManagerAcs.cpp
@@ -0,0 +1,40 @@
+#include "SecurityManagerAcs.h"
+
+#include <WinSock2.h>
+#include "openssl/ssl.h"
+
+void* SecurityManagerAcs::getSecureNeworkContext()
+{
+    SSL_CTX* ctx;
+    SSL* ssl;
+    int res = 0;
+
+    const char* cipher_list = "TLS_AES_128_GCM_SHA256";
+    const SSL_METHOD* meth = TLS_client_method();
+    ctx = SSL_CTX_new(meth);
+    res = SSL_CTX_set_cipher_list(ctx, cipher_list);
+    res = SSL_CTX_set_min_proto_version(ctx, TLS1_3_VERSION);
+    res = SSL_CTX_use_certificate_file(ctx, "client.crt", SSL_FILETYPE_PEM);
+    res = SSL_CTX_use_PrivateKey_file(ctx, "client.key", SSL_FILETYPE_PEM);
+    res = SSL_CTX_check_private_key(ctx);
+    res = SSL_CTX_load_verify_locations(ctx, "rootca.crt", NULL);
+    SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, NULL); // mtls, fail if no cert of client
+    ssl = SSL_new(ctx);
+
+    secureNetworkContext = reinterpret_cast<void*>(ctx);
+    return (void*) ssl;
+}
+
+int SecurityManagerAcs::freeSecureNetworkContext(void* p)
+{
+    SSL_free((SSL*)p);
+    SSL_CTX_free((SSL_CTX*)secureNetworkContext);
+    return 0;
+}
+
+void SecurityManagerAcs::setNetworkSd(void* p, int sd)
+{
+    SSL* ssl = reinterpret_cast<SSL*>(p);
+    SSL_set_fd(ssl, sd);
+    SSL_connect(ssl);
+}
